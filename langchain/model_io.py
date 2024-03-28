f1c0dcfbba5a305fd93b3126bc79bd1b2762622a
kubealex <al.rossi87@gmail.com>
2024-03-28 17:48:27 +0100

    testing around

---
+++ langchain/model_io.py
@@ -0,0 +1,19 @@
+from langchain_community.llms import Ollama
+from langchain_community.chat_models import ChatOllama
+from langchain.prompts.chat import ChatPromptTemplate
+
+llm = Ollama(model="llama2")
+chat_model = ChatOllama()
+
+template = "You are a helpful assistant that translates {input_language} to {output_language}."
+human_template = "{text}"
+
+chat_prompt = ChatPromptTemplate.from_messages([
+    ("system", template),
+    ("human", human_template),
+])
+
+messages = chat_prompt.format_messages(input_language="English", output_language="Italian", text="I love programming.")
+
+response = chat_model.invoke(messages)
+print(response)
