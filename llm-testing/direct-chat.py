0a4f7c0350d9de191791bd8ee31122c1b4b5122b
kubealex <al.rossi87@gmail.com>
2024-03-28 15:33:52 +0100

    add some tests

---
+++ llm-testing/direct-chat.py
@@ -0,0 +1,10 @@
+import ollama
+response = ollama.chat(model='llama2', messages=[
+  {
+    'role': 'user',
+    'content': 'Why is the sky blue?',
+  },
+])
+print(response['message']['content'])
+
+print(ollama.generate(model='llama2', prompt='What is an LLM?'))
---
+++ llm-testing/generate.py
@@ -0,0 +1,4 @@
+import ollama
+
+response = ollama.generate(model='llama2', prompt='What is an LLM?', stream=False)
+print(response['response'])
