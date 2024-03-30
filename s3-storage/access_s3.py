7d4fe4b4ae32b4e98d6162fd9009527a480455d8
kubealex <al.rossi87@gmail.com>
2024-03-30 18:49:32 +0100

    dedicated library

--- s3-storage/access_s3.py
+++ s3-storage/access_s3.py
@@ -1,16 +1,18 @@
 import boto3
-import os
+from environs import Env
 
-s3_endpoint = os.getenv("AWS_S3_ENDPOINT")
-s3_access_key = os.getenv("AWS_S3_ACCESS_KEY")
-s3_secret_token = os.getenv("AWS_S3_SECRET_TOKEN")
+env = Env()
+env.read_env()
+s3_endpoint = env("AWS_S3_ENDPOINT")
+s3_access_key = env("AWS_S3_ACCESS_KEY")
+s3_secret_token = env("AWS_S3_SECRET_TOKEN")
 
 session = boto3.session.Session()
 s3_client = session.client(
     endpoint_url=s3_endpoint,
     service_name="s3",
     aws_access_key_id=s3_access_key,
-    aws_secret_access_key=s3_secret_token,
+    aws_secrßßet_access_key=s3_secret_token,
     use_ssl=True,
     verify=False,
 )
