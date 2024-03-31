b0aa767bcfb39e1e6b5c59c9f3ccc3c55c40ec0d
kubealex <al.rossi87@gmail.com>
2024-03-31 19:54:33 +0200

    add args

--- intregrations/telegram_integration.py
+++ intregrations/telegram_integration.py
@@ -1,6 +1,8 @@
-from telegram import Update
-from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+import os
+
 from environs import Env
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 
 
 def initialize_vars():
@@ -13,14 +15,21 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f"Hello {update.effective_user.first_name}")
 
 
+async def list(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    user_input = " ".join(context.args)
+    dirlisåt = os.listdir(user_input)
+    await update.message.reply_text(f"{dirlist}")
+
+
 def main():
     env = initialize_vars()
     telegram_token = env("TELEGRAM_TOKEN")
-    application = ApplicationBuilder()
     application = ApplicationBuilder().token(telegram_token).build()
 
     start_handler = CommandHandler("hello", hello)
-    application.add_handler(start_handler)
+    list_handler = CommandHandler("list", list)
+
+    application.add_handlers([start_handler, list_handler])
 
     application.run_polling()
 
