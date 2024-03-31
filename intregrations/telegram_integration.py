3c8d45577be1f32139a4e72bd028850e1e478c5a
kubealex <al.rossi87@gmail.com>
2024-03-31 12:58:58 +0200

    testing around

--- intregrations/telegram_integration.py
+++ intregrations/telegram_integration.py
@@ -13,12 +13,17 @@ async def hello(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     await update.message.reply_text(f"Hello {update.effective_user.first_name}")
 
 
-env = initialize_vars()
-telegram_token = env("TELEGRAM_TOKEN")
-application = ApplicationBuilder()
-application = ApplicationBuilder().token(telegram_token).build()
+def main():
+    env = initialize_vars()
+    telegram_token = env("TELEGRAM_TOKEN")
+    application = ApplicationBuilder()
+    application = ApplicationBuilder().token(telegram_token).build()
 
-start_handler = CommandHandler("hello", hello)
-application.add_handler(start_handler)
+    start_handler = CommandHandler("hello", hello)
+    application.add_handler(start_handler)
 
-application.run_polling()
+    application.run_polling()
+
+
+if __name__ == "__main__":
+    main()
