8d95c9d87a46465c4537c035cc3de75d6bdacf31
kubealex <al.rossi87@gmail.com>
2024-07-14 19:48:47 +0200

    testing

--- python-ldap/ldap-example.py
+++ python-ldap/ldap-example.py
@@ -3,20 +3,17 @@ import argparse
 import json
 
 parser = argparse.ArgumentParser("LDAP Connection example")
-parser.add_argument('ldap_server', type=str, help="LDAP Server connection string", default="ldap://idm.rh-lab.labs", nargs="*")
+parser.add_argument("ldap_server", type=str, help="LDAP Server connection string", default="ldap://idm.rh-lab.labs", nargs="?")
+parser.add_argument("ldap_bind_dn", type=str, help="LDAP Server Bind DN for searches", default="uid=admin,cn=users,cn=accounts,dc=rh-lab,dc=labs", nargs="*")
+parser.add_argument("ldap_bind_pwd", type=str, help="LDAP Server Bind DN password for searches", default="admin123", nargs="*")
+parser.add_argument("ldap_search_base_dn", type=str, help="Base DN for the LDAP search", default="cn=users,cn=accounts,dc=rh-lab,dc=labs", nargs="*")
 args = parser.parse_args()
-
-print(args.ldap_server)
-
-_ldap = ldap.initialize(args.ldap_server,bytes_mode=False, bytes_strictness='silent')
-ldap_user="uid=admin,cn=users,cn=accounts,dc=rh-lab,dc=labs"
-ldap_pass="admin123"
-ldap_base="cn=users,cn=accounts,dc=rh-lab,dc=labs"
 ldap_scope = ldap.SCOPE_SUBTREE
 
 try:
-    _ldap.simple_bind_s(ldap_user,ldap_pass)
-    _ldap_search = _ldap.search(base=ldap_base, scope=ldap_scope)
+    _ldap = ldap.initialize(args.ldap_server,bytes_mode=False, bytes_strictness="silent")
+    _ldap.simple_bind_s(args.ldap_bind_dn, args.ldap_bind_pwd)
+    _ldap_search = _ldap.search(base=args.ldap_search_base_dn, scope=ldap_scope)
     type, user = _ldap.result(_ldap_search)
     print(user)
 except ldap.LDAPError as ldap_err: 
