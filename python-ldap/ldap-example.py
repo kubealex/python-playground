a7b445768b9eb72304a3193a9428a60352bf6676
kubealex <al.rossi87@gmail.com>
2024-07-14 19:32:33 +0200

    add params

--- python-ldap/ldap-example.py
+++ python-ldap/ldap-example.py
@@ -1,16 +1,23 @@
 import ldap
+import argparse
 import json
-_ldap = ldap.initialize("ldap://idm.rh-lab.labs",bytes_mode=False, bytes_strictness='silent')
+
+parser = argparse.ArgumentParser("LDAP Connection example")
+parser.add_argument('ldap_server', type=str, help="LDAP Server connection string", default="ldap://idm.rh-lab.labs", nargs="*")
+args = parser.parse_args()
+
+print(args.ldap_server)
+
+_ldap = ldap.initialize(args.ldap_server,bytes_mode=False, bytes_strictness='silent')
 ldap_user="uid=admin,cn=users,cn=accounts,dc=rh-lab,dc=labs"
 ldap_pass="admin123"
 ldap_base="cn=users,cn=accounts,dc=rh-lab,dc=labs"
 ldap_scope = ldap.SCOPE_SUBTREE
 
-_ldap.simple_bind(ldap_user,ldap_pass)
-_ldap_search = _ldap.search(base=ldap_base, scope=ldap_scope)
-type, user = _ldap.result(_ldap_search)
-for a, _user in user:
-    print(_user.__class__)
-    for _attr in _user.values():
-        print(b' '.join(_attr).decode('utf-8'))
-        print(_attr.__class__)
+try:
+    _ldap.simple_bind_s(ldap_user,ldap_pass)
+    _ldap_search = _ldap.search(base=ldap_base, scope=ldap_scope)
+    type, user = _ldap.result(_ldap_search)
+    print(user)
+except ldap.LDAPError as ldap_err: 
+    print(ldap_err)
